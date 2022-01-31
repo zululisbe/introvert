27e39acd5393ae6c202f87c2f07af567dbd9b113
zoeestewart <zuluoscarecho@gmail.com>
2022-01-31 12:24:01 +0300

    Added styles and more

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ var excuse = [`"I really appreciate the offer, but I'm taking the day/morning/af
 `"What a bummer—I have another commitment. Hope the event is a success!"`,
 `"I already have plans at that exact time. Can we schedule a catch-up for another date soon?"`,
 `"I wish I could come, but I'm busy that afternoon. I know that it will be a success and I am sorry to miss out."`,
-`"Ah, that sounds lovely, but I have a rule that I don’t make plans on Sundays—it’s my day [to decompress and sexually service my husband][catch up with my poisoning of the neighbours dogs][dress my gimp in a french maid outfit]. But thank you for thinking of me!"`,
+`"Ah, that sounds lovely, but I have a rule that I don’t make plans on Sundays—it’s my day [to decompress and sexually service my husband][catch up with my poisoning the neighbours dogs][dress my gimp in a french maid outfit]. But thank you for thinking of me!"`,
 `"Oh, that sounds like a blast, but I’m pretty committed to my 9 p.m. sleep schedule on weeknights these days. But thank you for the invite!"`,
-`"[Catching the rona/fancy meals out/charity events] isn’t really my thing, so I’m going to sit this one out!"`,
+`"[Catching the RONA/fancy meals out/charity events] isn’t really my thing, so I’m going to sit this one out!"`,
 `"Thank you for thinking of me, but [large gatherings/ botox parties] aren’t really my speed!"`,
 ];
 
 function newExcuse () {
 	var randomNumber = Math.floor(Math.random() * (excuse.length));
-	document.getElementById('excuse').innerHTML = "You can say: " + " " + excuse[randomNumber];
+	document.getElementById('excuse').innerHTML =  excuse[randomNumber];
 }
 
 newExcuse();
